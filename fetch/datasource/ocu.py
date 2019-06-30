61f57b7b6b1fcbe3f24a9dbb2c937e1ca082e12d
Ángel Guzmán Maeso <shakaran@gmail.com>
2019-06-30 04:18:08 +0200

    <feature> Improve remain values in page static

--- fetch/datasource/ocu.py
+++ fetch/datasource/ocu.py
@@ -17,7 +17,7 @@ class OcuFetcher():
 
         if page.status_code == 200:
 
-            soup = BeautifulSoup(page.content, 'html.parser')
+            soup = BeautifulSoup(page.content.decode('utf-8', 'ignore'), 'html.parser')
 
             # MAGIC HERE
             listing = soup.find('div', attrs={'data-type' : 'PsfProductListController'})
@@ -41,7 +41,63 @@ class OcuFetcher():
                             print('No title')
                             continue
 
-                        self.data.append({'title': data_title, 'link': data_link})
+                        quality_overall = element.find('span', attrs={'class' : 'quality-badge__value'})
+
+                        if quality_overall:
+                            data_quality_overall = quality_overall.text.strip()
+                            print('Quality Overall: ' + data_quality_overall)
+
+                        quality_overall_info = element.find('span', attrs={'class' : 'quality-badge__info'})
+
+                        if quality_overall_info:
+                            data_quality_overall_info = quality_overall_info.text.strip().replace('CALIDAD', '')
+                            print('Quality Overall Info: ' + data_quality_overall_info)
+
+                        specs = element.find('div', attrs={'class' : 'recommended__listing__item__specs'})
+
+                        if specs:
+                            data_specs = specs.find_all('p')
+
+                            if data_specs[0]:
+                                data_spec_content = data_specs[0].text.replace('Contenido: ', '').strip()
+                                print('Content: ' + data_spec_content)
+
+                            if data_specs[1]:
+                                data_spec_spf = data_specs[1].text.replace('SPF: ', '').strip()
+                                print('SPF: ' + data_spec_spf)
+
+                            if len(data_specs) > 2:
+                                data_spec_container = data_specs[2].text.replace('Precio por envase: ', '').strip()
+                                print('Price by container: ' + data_spec_container)
+
+
+                        provider_value = element.find('div', attrs={'class' : 'recommended__calltoaction__provider-value'})
+
+                        if specs:
+                            data_provider_value = provider_value.text
+
+                            print('Provider value: ' + str(data_provider_value.encode('utf-8').strip()))
+
+                        picture_image = element.find('a', attrs={'class' : 'recommended__picture-image'})
+
+                        if picture_image:
+                            data_picture_image = picture_image.find('img').get('src').strip()
+                            print('Picture Image: ' + data_picture_image)
+
+
+
+                        self.data.append(
+                            {
+                                'title': data_title,
+                                'link': data_link,
+                                'quality_overall': data_quality_overall,
+                                'quality_overall_info': data_quality_overall_info,
+                                'spec_content': data_spec_content,
+                                'spec_spf': data_spec_spf,
+                                'spec_container': data_spec_container,
+                                'provider_value': data_provider_value,
+                                'picture_image': data_picture_image,
+                            })
 
                         print
 
